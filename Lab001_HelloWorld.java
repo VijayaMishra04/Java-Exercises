7c90132a2294ed3ab1c7bf50f27d9c9213a80874
VijayaMishra04 <mishravijjo4@gmail.com>
2024-10-10 13:14:50 +0530

    Lab001_HelloWorld.java

--- Lab001_HelloWorld.java
+++ Lab001_HelloWorld.java
@@ -2,6 +2,6 @@ package Oct.Ex_02_10_2024_MainMethod;
 
 public class Lab001 {
     public static void main(String[] args){
-        System.out.println("Hello All");
+        System.out.println("Hello World");
     }
 }
